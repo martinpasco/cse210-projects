5f01a8abdfe166bd8b158a3be1feb765148afac0
martinpasco <martinpasco@hotmail.com>
2025-03-08 15:54:05 -0700

    update
    updte

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your Name?  ");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your Lastname?");
+        string Lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {name}, {name} {Lastname}");
     }
 }
