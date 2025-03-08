442dcc26a61688a2179ad93572461ee369109eaa
martinpasco <martinpasco@hotmail.com>
2025-03-08 16:02:04 -0700

    description
    test program excersice2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is ypur avegaje in percent");
+        int answer = int.Parse(Console.ReadLine());
+
+        if (answer <= 90)
+        {
+            Console.WriteLine("Your letter grade is A");
+        }
+        else if (answer > 90)
+        {
+            Console.WriteLine("Your letter grade is B");
+        }
+        else if (answer <= 80)
+        {
+            Console.WriteLine("Your letter grade is C");
+        }
+        else if (answer > 80)
+        {
+            Console.WriteLine("Your letter grade is D");
+        }
+        else if (answer <= 70)
+        {
+            Console.WriteLine("Your letter grade is F");
+        }
+        else
+        {
+            Console.WriteLine("Wrong Number");
+        }
+
+        Console.WriteLine($"Your grade is : {answer}");
+        if (answer >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You need More dedication, not passed!");
+        }
     }
 }
