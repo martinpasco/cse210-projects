7e7c57ee861fea075ea0e736347e7554e58815f6
martinpasco <martinpasco@hotmail.com>
2025-03-13 22:04:09 -0600

    cration job.cs and resume.cs and modify program.cs

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+    public string _startMonth;
+    public string _endMonth;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) from {_startMonth} until {_endMonth} {_startYear}-{_endYear}");
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,8 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        job1._startMonth = "January";
+        job1._startMonth = "April";
 
-        sdf
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+        job2._startMonth = "March";
+        job2._startMonth = "December";
+
+        Resume myResume = new Resume();
+        myResume._name = "Martin";
+        myResume._lastname = "Pasco";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
